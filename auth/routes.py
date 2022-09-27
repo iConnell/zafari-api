687b66483dd7db27057dce94c27e30e5504817af
McConnell <ik.ugwuanyi@gmail.com>
2022-09-27 22:54:18 +0100

    login route

--- auth/routes.py
+++ auth/routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from . import schemas, models
 from .database import get_db
-from .utils import hash_password, create_access_token
+from .utils import hash_password, verify_password, create_access_token
 
 
 router = APIRouter(
@@ -24,7 +24,16 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
 
-    expires_in = timedelta(minutes=30)
-    return {"token":create_access_token({"email": new_user.email, "id":new_user.id}, expires_in)}
+    return {"token":create_access_token({"email": new_user.email, "id":new_user.id})}
 
 
+@router.post('/login', status_code=status.HTTP_200_OK)
+def user_login(credentials: schemas.UserCreate, db: Session=Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email==credentials.email).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials provied")
+    
+    if not verify_password(credentials.password, user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials provied")
+
+    return {"token":create_access_token({"email": user.email, "id":user.id})}
